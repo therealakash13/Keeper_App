c50ececf7ed2e6e4cd0f0f2479c4e80626794396
Akash <therealakash13@gmail.com>
2025-11-03 14:27:00 +0530

    initialize keeper (g keep clone)

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+export default App;
---
+++ src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <p>Copyright @ <span>{new Date().getFullYear()}</span></p>
+    </footer>
+  );
+}
+
+export default Footer;
