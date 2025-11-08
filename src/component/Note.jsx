9262448050572c99da89083b2b51c107b29c823b
Akash <therealakash13@gmail.com>
2025-11-08 18:06:11 +0530

    delete logic

--- src/component/Note.jsx
+++ src/component/Note.jsx
@@ -1,9 +1,13 @@
 function Note(props) {
+  function handleDelete(e) {
+    e.preventDefault();
+    props.delete(props.id);
+  }
   return (
     <div className="note">
       <h1>{props.title}</h1>
       <p>{props.content}</p>
-      <button>
+      <button onClick={handleDelete}>
         <span class="material-symbols-outlined">delete_forever</span>
       </button>
     </div>
